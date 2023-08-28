8e5ed18a1aef0c9abf023609d4ad946af67fc55e
Arjin Ateş <arjinates@hotmail.com>
2023-08-28 16:49:53 +0300

    Program.cs services added for database and addAutoMapper service fixed

--- Services/FreeCourse.Services.Catalog/Program.cs
+++ Services/FreeCourse.Services.Catalog/Program.cs
@@ -1,13 +1,23 @@
+using FreeCourse.Services.Catalog.Settings;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<IDatabaseSettings>>().Value;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
