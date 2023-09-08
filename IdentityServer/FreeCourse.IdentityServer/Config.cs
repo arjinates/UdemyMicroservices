ad60b2a5f2b1935fcc694eaa041becde65e1cca5
Arjin Ateş <arjinates@hotmail.com>
2023-09-08 09:40:59 +0300

    add: refresh token and IdentityResources

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -4,6 +4,7 @@
 
 using IdentityServer4;
 using IdentityServer4.Models;
+using System;
 using System.Collections.Generic;
 
 namespace FreeCourse.IdentityServer
@@ -18,9 +19,12 @@ namespace FreeCourse.IdentityServer
         };
 
         public static IEnumerable<IdentityResource> IdentityResources =>
-            new IdentityResource[]
+            new IdentityResource[] //
             {
-
+                new IdentityResources.Email(), //emailClaim hazir geliyor
+                new IdentityResources.OpenId(), //JWT'nin payloadindaki sub claimine karsilik geliyor
+                new IdentityResources.Profile(), //profile claim
+                new IdentityResource(){Name="roles",DisplayName="Roles",Description="User Roles",UserClaims=new[]{"role"}}
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -36,11 +40,25 @@ namespace FreeCourse.IdentityServer
             {
                 new Client
                 {
-                    ClientName = "AspNetCoreMvc",
+                    ClientName = "Asp.Net Core MVC",
                     ClientId = "WebMvcClient",
                     ClientSecrets = {new Secret("secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     AllowedScopes = {"catalog_fullpermission","photo_stock_fullpermission",IdentityServerConstants.LocalApi.ScopeName},
+                },
+                new Client
+                {
+                    ClientName = "AspNetCoreMvc",
+                    ClientId = "WebMvcClientForUser",
+                    ClientSecrets = {new Secret("secret".Sha256())},    
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,               //OpenId'ye mutlaka ulasmak durumunda
+                    AllowedScopes = {IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.OfflineAccess,"roles"},
+                    AccessTokenLifetime=1*60*60,
+                    RefreshTokenExpiration=TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime = (int) (DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
+                    RefreshTokenUsage = TokenUsage.ReUse
+
                 }
             };
     }
