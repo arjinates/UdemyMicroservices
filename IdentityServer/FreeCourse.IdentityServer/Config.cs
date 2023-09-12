816b0858c33d56803893d552754a33faef1895e9
Arjin Ateş <arjinates@hotmail.com>
2023-09-12 16:49:45 +0300

    add: apiScope and ApiResource for Basket mcroservice fix: catalog microservice apiResource name

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -14,7 +14,8 @@ namespace FreeCourse.IdentityServer
         public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
         {
             new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
-            new ApiResource("photo_stock_catalog"){Scopes={"photo_stock_fullpermission"}},
+            new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
+            new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -30,8 +31,9 @@ namespace FreeCourse.IdentityServer
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
-                new ApiScope("catalog_fullpermission","Catalog API için full erisim"),
-                new ApiScope("photo_stock_fullpermission","Photo Stock API için full erisim"),
+                new ApiScope("catalog_fullpermission","Catalog API icin full erisim"),
+                new ApiScope("photo_stock_fullpermission","Photo Stock API icin full erisim"),
+                new ApiScope("basket_fullpermission","Basket API icin full erisim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -53,7 +55,7 @@ namespace FreeCourse.IdentityServer
                     AllowOfflineAccess = true,
                     ClientSecrets = {new Secret("secret".Sha256())},    
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,               //OpenId'ye mutlaka ulasmak durumunda
-                    AllowedScopes = {IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,
+                    AllowedScopes = {"basket_fullpermission",IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,
                     IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.OfflineAccess,
                         IdentityServerConstants.LocalApi.ScopeName,"roles"},
                     AccessTokenLifetime=1*60*60,
--- Services/Basket/FreeCourse.Services.Basket/Program.cs
+++ Services/Basket/FreeCourse.Services.Basket/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddControllers(opt =>
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
     options.Authority = builder.Configuration["IdentityServerURL"];
-    options.Audience = "TODO";
+    options.Audience = "basket_fullpermission";
     options.RequireHttpsMetadata = false;
 });
 
