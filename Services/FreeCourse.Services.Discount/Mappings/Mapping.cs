3424ea7bb06a358b20572d7db9650edf38a72a71
Arjin Ateş <arjinates@hotmail.com>
2023-09-26 11:32:39 +0300

    add: AutoMapper, DiscountDto

---
+++ Services/FreeCourse.Services.Discount/Mappings/Mapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Discount.Dtos;
+using FreeCourse.Services.Discount.Models;
+
+namespace FreeCourse.Services.Discount.Mappings
+{
+    public class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<DiscountDto, Models.Discount>().ReverseMap();
+        }
+    }
+}
