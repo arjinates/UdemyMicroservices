1f1f0c85b93d7d805e85ebfd805185bb8c2fb923
Arjin Ateş <arjinates@hotmail.com>
2023-09-26 11:33:17 +0300

    add: IDiscountService

---
+++ Services/FreeCourse.Services.Discount/Services/IDiscountService.cs
@@ -0,0 +1,15 @@
+﻿using FreeCourse.Services.Discount.Dtos;
+using FreeCourse.Shared.Dtos;
+
+namespace FreeCourse.Services.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<Response<List<DiscountDto>>> GetAll();
+        Task<Response<DiscountDto>> GetById(int id);
+        Task<Response<NoContent>> Save(DiscountDto dto);
+        Task<Response<NoContent>> DeleteById(int id);
+        Task<Response<NoContent>> Update(DiscountDto dto);
+        Task<Response<DiscountDto>> GetByCodeAndUserId(string code, string userId);
+    }
+}
