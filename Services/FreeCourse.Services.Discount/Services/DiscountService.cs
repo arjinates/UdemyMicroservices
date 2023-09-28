5f89a159a90b0f7e69e97a269818f7e66b496269
Arjin Ateş <arjinates@hotmail.com>
2023-09-28 11:59:24 +0300

    fix: endpoints fix: IdentityServerURL address

--- Services/FreeCourse.Services.Discount/Services/DiscountService.cs
+++ Services/FreeCourse.Services.Discount/Services/DiscountService.cs
@@ -25,13 +25,13 @@ namespace FreeCourse.Services.Discount.Services
 
         public async Task<Response<NoContent>> DeleteById(int id)
         {
-            if ((await _dbConnection.QueryAsync("Select * from discount where id=@Id", new { Id=id })).FirstOrDefault())
+FixedArgumentsEncoder:            if ((await _dbConnection.QueryAsync("Select * from discount where id=@Id", new { Id=id })).FirstOrDefault() == null)
             {
                 return Response<NoContent>.Fail("Discount could not found!", 404);
             }
             var status = await _dbConnection.ExecuteAsync("DELETE FROM discount WHERE id=@Id", new { Id=id });
 
-            return status > 0 ? Response<NoContent>.Fail("An ERROR occured while deleting.", 500) : Response<NoContent>.Success(204);
+            return status>0 ? Response<NoContent>.Success(204)  : Response<NoContent>.Fail("An ERROR occured while deleting.", 500);
         }
 
         public async Task<Response<List<DiscountDto>>> GetAll()
@@ -46,34 +46,33 @@ namespace FreeCourse.Services.Discount.Services
             {
                 return Response<DiscountDto>.Fail("Code and userId can not be empty.", 400);
             }
-            var discount = (await _dbConnection.QueryAsync("SELECT * FROM discount WHERE code=@Code and userid=@UserId",
+            var discount = (await _dbConnection.QueryAsync<Models.Discount>("SELECT * FROM discount WHERE code=@Code and userid=@UserId",
                 new { Code = code, UserId = userId })).SingleOrDefault();
 
             return discount == null ? Response<DiscountDto>.Fail("Discount could not found.", 404) : 
-                Response<DiscountDto>.Success(_mapper.Map<DiscountDto>(discount));
+                Response<DiscountDto>.Success(_mapper.Map<DiscountDto>(discount), 200);
         }
 
         public async Task<Response<DiscountDto>> GetById(int id)
         {
-            var discount = (await _dbConnection.QueryAsync("Select * from discount where id=@Id", new { Id = id })).SingleOrDefault();
-            
+            var discount = (await _dbConnection.QueryAsync<Models.Discount>("Select * from discount where id=@Id", new { Id=id })).SingleOrDefault();
             return discount==null ? Response<DiscountDto>.Fail("Discount could not found", 404) :
                 Response<DiscountDto>.Success(_mapper.Map<DiscountDto>(discount), 200);
         }
 
         public async Task<Response<NoContent>> Save(DiscountDto dto)
         {
-            var status = await _dbConnection.ExecuteAsync("INSERT INTO discount (userid,rate,code) VALUES(@UserId,@Rate,@Code)", dto);
+            var status = await _dbConnection.ExecuteAsync("INSERT INTO discount (userid,rate,code) VALUES(@UserId,@Rate,@Code)", _mapper.Map<Models.Discount>(dto));
             return status>0 ? Response<NoContent>.Success(204) : Response<NoContent>.Fail("An ERROR occured while adding",500);
         }
 
         public async Task<Response<NoContent>> Update(DiscountDto dto)
         {
-            if ((await _dbConnection.QueryAsync("Select * from discount where id=@Id", new { Id=dto.Id })).FirstOrDefault())
+            if ((await _dbConnection.QueryAsync("Select * from discount where id=@Id", new { Id=dto.Id })).FirstOrDefault()==null)
             {
                 return Response<NoContent>.Fail("Discount could not found!", 404);
             }
-            var status = await _dbConnection.ExecuteAsync("update discount set userid=@UserId, code=@Code, rate=@Rate wherer id=@Id",
+            var status = await _dbConnection.ExecuteAsync("update discount set userid=@UserId, code=@Code, rate=@Rate where id=@Id",
                 new { Id=dto.Id, UserId=dto.UserId, Rate=dto.Rate, Code=dto.Code }); //tek tek de verebilirim
 
             return status>0 ? Response<NoContent>.Success(204) : Response<NoContent>.Fail("An ERROR occured while updating", 500);
