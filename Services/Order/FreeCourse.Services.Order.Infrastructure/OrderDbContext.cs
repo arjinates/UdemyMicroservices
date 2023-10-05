f61b2844cd7759a6a427be8b755ef2eeebe58d53
Arjin Ateş <arjinates@hotmail.com>
2023-10-05 11:37:30 +0300

    fix2

--- Services/Order/FreeCourse.Services.Order.Infrastructure/OrderDbContext.cs
+++ Services/Order/FreeCourse.Services.Order.Infrastructure/OrderDbContext.cs
@@ -22,7 +22,7 @@ namespace FreeCourse.Services.Order.Infrastructure
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Domain.OrderAggregate.Order>().ToTable("Orders", DEFAULT_SCHEMA);
-            modelBuilder.Entity<OrderItem>().ToTable("Orders", DEFAULT_SCHEMA);
+            modelBuilder.Entity<OrderItem>().ToTable("OrderItems", DEFAULT_SCHEMA);
 
             modelBuilder.Entity<OrderItem>().Property(x => x.Price).HasColumnType("decimal(18,2)");
 
