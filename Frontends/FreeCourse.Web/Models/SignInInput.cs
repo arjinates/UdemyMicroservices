e48349e2a7c9f3aee29cdbf0e18a3c933fc3ee7e
Arjin Ateş <arjinates@hotmail.com>
2023-10-13 12:17:54 +0300

    add: SignInInput and IIdentityService

---
+++ Frontends/FreeCourse.Web/Models/SignInInput.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models
+{
+    public class SignInInput
+    {
+        [Display(Name = "Your email address")]
+        public string Email { get; set; }
+
+        [Display(Name = "Your password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool IsRemember { get; set; }
+    }
+}
